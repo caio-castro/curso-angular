3cc5c775b5488b7f2ce123ec2904fa6e060c6952
Caio Castro <caio-rb@hotmail.com>
2021-06-14 21:36:46 -0300

    Formulários e templates

--- src/app/Cursos/curso-info.component.ts
+++ src/app/Cursos/curso-info.component.ts
@@ -1,14 +1,17 @@
+import { CursoService } from './curso.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Curso } from './curso';
 
 @Component({
   templateUrl: './curso-info.component.html'
 })
 export class CursoInfoComponent implements OnInit {
 
-  cursoId!: number
+  curso!: Curso;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute,
+              private cursoService : CursoService) {
 
   }
 
@@ -16,9 +19,13 @@ export class CursoInfoComponent implements OnInit {
     let id  = this.activatedRoute.snapshot.paramMap.get('id');
 
     if(id != null ){
-      this.cursoId = +id;
+      this.curso = this.cursoService.retrieveById(+id);
     }
 
+  }
+
+  save():void{
+    this.cursoService.save(this.curso);
   }
 
 }
--- src/app/Cursos/curso.service.ts
+++ src/app/Cursos/curso.service.ts
@@ -4,74 +4,85 @@ import { Curso } from "./curso";
 @Injectable({
   providedIn: 'root'
 })
-export class CursoService{
+export class CursoService {
 
-  retrieveAll(): Curso[]{
+  retrieveAll(): Curso[] {
     return CURSOS;
+  }
+
+  retrieveById(id: number): any {
+    return CURSOS.find(cursoIteraitor => cursoIteraitor.id === id);
+  }
+
+  save(curso: Curso): void {
+
+    if (curso.id) {
+       const index = CURSOS.findIndex((cursoIteraitor : Curso )=> cursoIteraitor.id === curso.id)
+       CURSOS[index] = curso;
+    }
   }
 
 }
 
-
 var CURSOS: Curso[] = [
   {
-      id: 1,
-      nome: 'Angular: CLI',
-      dataLancamento: 'November 2, 2019',
-      descricao: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
-      duracao: 120,
-      codigo: 'XLF-1212',
-      avaliacao: 3,
-      preco: 12.99,
-      imagemUrl: '/assets/imagens/cli.png',
-      opcao: '',
+    id: 1,
+    nome: 'Angular: CLI',
+    dataLancamento: 'November 2, 2019',
+    descricao: 'Neste curso, os alunos irão obter um grande conhecimento nos principais recursos do CLI.',
+    duracao: 120,
+    codigo: 'XLF-1212',
+    avaliacao: 3,
+    preco: 12.99,
+    imagemUrl: '/assets/imagens/cli.png',
+    opcao: '',
   },
   {
-      id: 2,
-      nome: 'Angular: Forms',
-      dataLancamento: 'November 4, 2019',
-      descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
-      duracao: 80,
-      codigo: 'DWQ-3412',
-      avaliacao: 3.5,
-      preco: 24.99,
-      imagemUrl: '/assets/imagens/forms.png',
-      opcao: '',
+    id: 2,
+    nome: 'Angular: Forms',
+    dataLancamento: 'November 4, 2019',
+    descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Forms.',
+    duracao: 80,
+    codigo: 'DWQ-3412',
+    avaliacao: 3.5,
+    preco: 24.99,
+    imagemUrl: '/assets/imagens/forms.png',
+    opcao: '',
   },
   {
-      id: 3,
-      nome: 'Angular: HTTP',
-      dataLancamento: 'November 8, 2019',
-      descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
-      duracao: 80,
-      codigo: 'QPL-0913',
-      avaliacao: 4.0,
-      preco: 36.99,
-      imagemUrl: '/assets/imagens/http.png',
-      opcao: '',
+    id: 3,
+    nome: 'Angular: HTTP',
+    dataLancamento: 'November 8, 2019',
+    descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de HTTP.',
+    duracao: 80,
+    codigo: 'QPL-0913',
+    avaliacao: 4.0,
+    preco: 36.99,
+    imagemUrl: '/assets/imagens/http.png',
+    opcao: '',
   },
   {
-      id: 4,
-      nome: 'Angular: Router',
-      dataLancamento: 'November 16, 2019',
-      descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
-      duracao: 80,
-      codigo: 'OHP-1095',
-      avaliacao: 4.5,
-      preco: 46.99,
-      imagemUrl: '/assets/imagens/router.png',
-      opcao: '',
+    id: 4,
+    nome: 'Angular: Router',
+    dataLancamento: 'November 16, 2019',
+    descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis no módulo de Router.',
+    duracao: 80,
+    codigo: 'OHP-1095',
+    avaliacao: 4.5,
+    preco: 46.99,
+    imagemUrl: '/assets/imagens/router.png',
+    opcao: '',
   },
   {
-      id: 5,
-      nome: 'Angular: Animations',
-      dataLancamento: 'November 25, 2019',
-      descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
-      duracao: 80,
-      codigo: 'PWY-9381',
-      avaliacao: 5,
-      preco: 56.99,
-      imagemUrl: '/assets/imagens/animations.png',
-      opcao: '',
+    id: 5,
+    nome: 'Angular: Animations',
+    dataLancamento: 'November 25, 2019',
+    descricao: 'Neste curso, os alunos irão obter um conhecimento aprofundado sobre os recursos disponíveis sobre Animation.',
+    duracao: 80,
+    codigo: 'PWY-9381',
+    avaliacao: 5,
+    preco: 56.99,
+    imagemUrl: '/assets/imagens/animations.png',
+    opcao: '',
   }
 ];
